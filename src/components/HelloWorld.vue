e8987da88dfa627e4e8d323939d2a7981b74ed14
张进 <zhangjin@zhangjindeMacBook-Pro.local>
2018-06-14 10:22:16 +0800

    master branch

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <input type="text" v-model="msg">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1{
+        font-size: 20px;
+        color: #42b983;
+    }
+
+</style>
